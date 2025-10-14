5c42f10bd48613b7acfbd16b11737c90bb7a2a48
Jozef Wagner <jozef.wagner@gmail.com>
2025-10-14 19:26:36 +0200

    [#2] Vytvorenie aliasov v __init__.py

---
+++ tvary/__init__.py
@@ -0,0 +1,3 @@
+from .kruh import obvod as obvod_kruhu, obsah as obsah_kruhu
+from .obdlznik import obvod as obvod_obdlznika, obsah as obsah_obdlznika
+from .stvorec import obvod as obvod_stvorca, obsah as obsah_stvorca
